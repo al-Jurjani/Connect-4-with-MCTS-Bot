df3b1a803b5e3bf6f1613df9a02ae98f6c23d3a9
Zuhair binFarhan <zuhairfarhan@gmail.com>
2024-04-11 14:59:44 +0500

    Added token insertion methods.

--- Game/connect4.py
+++ Game/connect4.py
@@ -1,11 +1,9 @@
-import array as arr
 import numpy as np
 class connect4:
-
-
     redWins = 0
     yellowWins = 0
     playAgain = 'y'
+    slotsRemaining = 42
     board = np.array([[' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -15,6 +13,48 @@ class connect4:
     
     def __init__(self) -> None:
         pass
+
+    def playGame(self):
+        col = -1
+        curPlayer = ' '
+        while(self.slotsRemaining > 0):
+            self.printBoard()
+            curPlayer = self.curPlayer()
+            col = self.colPos()
+            self.enterToken(col, curPlayer)
+            self.slotsRemaining -= 1
+        self.printBoard()
+        print("All slots filled!")
+
+    def curPlayer(self):
+        if self.slotsRemaining % 2 == 0:
+            print("Player 1's turn. Token = 'X'")
+            return 'X'
+        else:
+            print("Player 1's turn. Token = 'O'")
+            return 'O'
+    
+    def colPos(self):
+        try:
+            col = int(input("Choose what column to insert your token in: 1-7\n")) - 1
+            if col < 0 or col > 6:
+                print("Please enter a number between 1 and 7.")
+                return self.colPos()
+            if self.board[0][col] != ' ':
+                print("The column is already full! Enter into another column!")
+                return self.colPos()
+            return col
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 7.")
+            return self.colPos()
+
+    def enterToken(self, col, curPlayer):
+        for i in range(5, -1, -1):
+            if self.board[i][col] == ' ':
+                self.board[i][col] = curPlayer
+                break
+        
+
 
     def printBoard(self):
         print("    1     2    3    4    5    6    7   ")
@@ -27,4 +67,4 @@ class connect4:
         print("  ____________________________________\n\n")
 
 game = connect4()
-game.printBoard()
+game.playGame()
