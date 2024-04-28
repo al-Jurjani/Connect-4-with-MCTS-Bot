b7d7afabcc257ea90ecfec47bf7d0d748e492a7d
Zuhair binFarhan <zuhairfarhan@gmail.com>
2024-04-28 14:47:44 +0500

    Improvements to base game.

--- Game/connect4.py
+++ Game/connect4.py
@@ -2,8 +2,6 @@ import numpy as np
 class connect4:
     redWins = 0
     yellowWins = 0
-    playAgain = 'y'
-    slotsRemaining = 42
     board = np.array([[' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
                       [' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -17,13 +15,16 @@ class connect4:
     def playGame(self):
         col = -1
         row = -1
+        height = np.array([6, 6, 6, 6, 6, 6, 6])
+        slotsRemaining = 42
         curPlayer = ' '
-        while(self.slotsRemaining > 0):
+        while(slotsRemaining > 0):
             self.printBoard()
-            curPlayer = self.curPlayer()
-            col = self.colPos()
-            row = self.enterToken(col, curPlayer)
-            self.slotsRemaining -= 1
+            curPlayer = self.curPlayer(slotsRemaining)
+            col = self.colPos(height)
+            row = self.enterToken(col, height, curPlayer)
+            print(row)
+            slotsRemaining -= 1
 
             if self.checkVer(col, curPlayer) | self.checkHor(row, curPlayer) | self.checkDiag(row, col, curPlayer):
                 self.printBoard()
@@ -32,26 +33,26 @@ class connect4:
                     self.redWins += 1
                 else:
                     self.yellowWins += 1
-                self.playAgain()
-            elif (self.slotsRemaining == 0):
+                slotsRemaining, height = self.playAgain()
+            elif (slotsRemaining == 0):
                 print("All slots filled! Game drawn!")
-                self.playAgain()
+                slotsRemaining, height = self.playAgain()
 
-    def curPlayer(self):
-        if self.slotsRemaining % 2 == 0:
+    def curPlayer(self, slotsRemaining):
+        if slotsRemaining % 2 == 0:
             print("Player 1's turn. Token = 'X'")
             return 'X'
         else:
             print("Player 2's turn. Token = 'O'")
             return 'O'
-    
-    def colPos(self):
+
+    def colPos(self, height):
         try:
             col = int(input("Choose what column to insert your token in: 1-7\n")) - 1
             if col < 0 or col > 6:
                 print("Please enter a number between 1 and 7.")
                 return self.colPos()
-            if self.board[0][col] != ' ':
+            if height[col] == 0:
                 print("The column is already full! Enter into another column!")
                 return self.colPos()
             return col
@@ -59,18 +60,17 @@ class connect4:
             print("Invalid input. Please enter a number between 1 and 7.")
             return self.colPos()
 
-    def enterToken(self, col, curPlayer):
-        for i in range(5, -1, -1):
-            if self.board[i][col] == ' ':
-                self.board[i][col] = curPlayer
-                return i
-        
+    def enterToken(self, col, height, curPlayer):
+        height[col] -= 1
+        self.board[height[col]][col] = curPlayer
+        return height[col]
+
     def checkVer(self, col, curPlayer):
         for i in range(0, 3):
             if self.board[i][col] == curPlayer and self.board[i+1][col] == curPlayer and self.board[i+2][col] == curPlayer and self.board[i+3][col] == curPlayer:
                 return True
         return False
-    
+
     def checkHor(self, row, curPlayer):
         for i in range(0, 3):
             if self.board[row][i] == curPlayer and self.board[row][i+1] == curPlayer and self.board[row][i+2] == curPlayer and self.board[row][i+3] == curPlayer:
@@ -91,7 +91,7 @@ class connect4:
                 # Check if new position is within the board boundaries
                 if 0 <= new_col < len(self.board[0]) and 0 <= new_row < len(self.board):
                     # Check if the token in the new position is the same as the current token
-                    if self.board[new_row][new_col] == self.board[row][col]:
+                    if self.board[new_row][new_col] == self.board[row][col] == curPlayer:
                         count += 1
                     else:
                         break  # Break if the consecutive streak is broken
@@ -99,18 +99,20 @@ class connect4:
                     break  # Break if the new position is out of bounds
             # Check if we have a winner
             if count >= 4:
+                print("true")
                 return True  # Return True if there are 4 or more consecutive tokens in a diagonal direction
+        print("false")
         return False  # Return False if no diagonal win is found
 
     def playAgain(self):
         response = input("Do you want to play again? (y/n)\n")
         if response.lower() in {'y', 'yes', 'ok', 'okay', 'k'}:
             self.initBoard()
-            self.slotsRemaining = 42
+            return 42, np.array([6, 6, 6, 6, 6, 6, 6])
         elif response.lower() in {'n', 'no', 'na', 'nop', 'nope'}:
             print("Red's total wins: " + str(self.redWins))
             print("Yellow's total wins: " + str(self.yellowWins))
-            self.slotsRemaining = 0
+            return 0, np.array([6, 6, 6, 6, 6, 6, 6])
 
     def initBoard(self):
         self.board = np.array([[' ', ' ', ' ', ' ', ' ', ' ', ' '],
